d9521b540fe9f61c49adae37281323f50b964f4d
Velovo <velovo.vanpraet@gmail.com>
2021-03-08 15:50:50 +0000

    update parser

--- reverse_proxy/parser.py
+++ reverse_proxy/parser.py
@@ -5,3 +5,6 @@ def parse(data, port, origin):
         return
     else:
         print "[" + origin + ":" + str(port) + "]" + data
+
+#il y a un lf ( + un cr) en fin de data ducoup c'est pour ca que ca print sur 2 lignes, faites un bail du genre pour print sur 1:
+#print ("[" + origin + ":" + str(port) + "]" + data, end='') je c meme pas si ca marche mais c'est un bail du genre pour sur
