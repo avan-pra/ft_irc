9814dea34d41ad25e4f6677dd588979c8887031b
Velovo <velovo.vanpraet@gmail.com>
2021-03-18 13:12:09 -0400

    building parser

--- srcs/parser.cpp
+++ srcs/parser.cpp
@@ -1,28 +1,59 @@
 #include <iostream>
 
-class Parser
+enum commandCode
 {
-	public:
-	std::string	_command;
-	size_t		_n_arg;
-	std::string	*_arg;
-
-	Parser(): _arg(NULL) { }
-	~Parser()
-	{
-		if (_arg != NULL)
-			delete [] _arg;
-	}
-	Parser &operator=(const Parser &parser);
+	eCAP,
+	eNICK,
+	ePRIVMSG,
+	eMODE,
+	ePING,
+	eERROR,
 };
 
-std::string *ft_split(std::string line, std::string charset);
+commandCode hashit(const std::string &inString)
+{
+	if (inString == "CAP") return eCAP;
+	if (inString == "NICK") return eNICK;
+	if (inString == "PRIVMSG") return ePRIVMSG;
+	if (inString == "MODE") return eMODE;
+	if (inString == "PING") return ePING;
+	return eERROR;
+}
 
-// void	parser(char *line)
-// {
-// 	std::string *packet;
-// 	size_t		i;
+// std::string *ft_split(std::string line, std::string charset); //a commenter / mettre dans un .h
 
-// 	packet = ft_split(std::string(line), std::string("\r\n"));
-// 	while (packet[i])
-// }
+void	parser(char *line)
+{
+	std::string	*packet;
+
+	// try
+	// {
+	// 	packet = ft_split(std::string(line), std::string("\r\n"));
+	// }
+	// catch(const std::exception& e) { throw std::exception(); }
+
+	for (size_t i = 0; packet[i] != std::string(""); ++i)
+	{
+		switch (hashit(packet[i].substr(0, packet[i].find(" ", 0))))
+		{
+			case eCAP:
+				// capfuntion()
+				break;
+			case eNICK:
+				// nickfuntion()
+				break;
+			case ePRIVMSG:
+				// privmsgfuntion()
+				break;
+			case eMODE:
+				// modefuntion()
+				break;
+			case ePING:
+				// pingfuntion()
+				break;
+			case eERROR:
+				break;
+		}
+	}
+	delete [] packet;
+}
