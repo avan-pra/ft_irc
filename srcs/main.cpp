34dab336ce40822d7b6d9d4939266967fc69d3f9
Jan eudald VAQUER <jvaquer@e1r7p20.42.fr>
2021-03-18 17:15:53 +0100

    Trying pairs

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 15:30:04 by lmoulin           #+#    #+#             */
-/*   Updated: 2021/03/18 15:31:52 by jvaquer          ###   ########.fr       */
+/*   Updated: 2021/03/18 16:34:36 by jvaquer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 SOCKET					g_serv_sock;
 std::vector<SOCKET>		g_cli_sock;
 std::vector<Client>		g_aClient;
+std::vector<std::pair<SOCKET, Client> >   g_tryPair;
 
 int			main(void)
 {
