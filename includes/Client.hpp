7f48191390719ed3768bae65351c503f07e8504e
Lucas MOULIN <lmoulin@e2r5p8.42.fr>
2021-03-19 12:31:19 +0100

    Clien

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 15:29:28 by jvaquer           #+#    #+#             */
-/*   Updated: 2021/03/18 15:53:25 by jvaquer          ###   ########.fr       */
+/*   Updated: 2021/03/19 12:31:10 by lmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,18 @@ class Client
 {
 	private:
 	
-		std::string _nickname;
-		std::string	_username;
-		std::string	_hostname;
-		std::string	_realname;
+		std::string		_nickname;
+		std::string		_username;
+		std::string		_hostname;
+		std::string		_realname;
+		std::string		_unended_packet;
 		//int			_status;
 	
 		//std::vector<Channel>	_channels;	
 	public:
 
-		SOCKET      *_fd;
-		sockaddr_in	sock_addr;
+		SOCKET			*_fd;
+		sockaddr_in		sock_addr;
 
 		Client()
 		{
