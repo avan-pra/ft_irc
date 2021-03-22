2f6403714a0300a5a59667e27a67023923bc76d3
lmoulin <lucas.moulin78114@gmail.com>
2021-03-22 16:32:05 +0100

    ft_split with vector of string

--- srcs/libft/ft_split.cpp
+++ srcs/libft/ft_split.cpp
@@ -6,11 +6,12 @@
 /*   By: lucas <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 15:17:11 by lucas             #+#    #+#             */
-/*   Updated: 2021/03/19 12:28:22 by lmoulin          ###   ########.fr       */
+/*   Updated: 2021/03/22 16:31:28 by lucas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <vector>
 
 int				len(std::string s, std::string splitter)
 {
@@ -30,21 +31,17 @@ int				len(std::string s, std::string splitter)
 	return (i + 2);
 }
 
-std::string		*ft_split(std::string s, std::string splitter)
+std::vector<std::string>		ft_split(std::string s, std::string splitter)
 {
-	std::string		*now = NULL;
-	std::string		tmp;
-	size_t			ret = 0;
-	size_t			i = 0;
-	size_t			k = 0;
+	std::vector<std::string>	now;
+	std::string					tmp;
+	size_t						ret = 0;
+	size_t						k = 0;
 
-	now = new std::string[len(s, splitter)];
 	ret = 0;
-	i = 0;
 	if (splitter.empty())
 	{
-		now[0] = s.substr(0);
-		now[1][0] = '\0';
+		now.push_back("");
 		return (now);
 	}
 	while (ret < s.length())
@@ -54,18 +51,16 @@ std::string		*ft_split(std::string s, std::string splitter)
 		if (k != ret)
 		{
 			if (ret == std::string::npos)
-				now[i] = s.substr(k);
+				now.push_back(s.substr(k));
 			else
 			{
-				now[i] = s.substr(k);
-				now[i].resize(ret - k);
+				now.push_back(s.substr(k));
+				now.back().resize(ret - k);
 			}
-			i++;
 		}
 		if (ret == std::string::npos)
 			break;
 		ret += splitter.length();
 	}
-	now[i] = "\0";
 	return (now);
 }
