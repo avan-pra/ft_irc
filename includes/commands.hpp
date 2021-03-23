d4009e1a4680b6502401a726085c788534a5a88f
lmoulin <lucas.moulin78114@gmail.com>
2021-03-23 12:03:05 +0100

    add join_command to command.hpp

--- includes/commands.hpp
+++ includes/commands.hpp
@@ -147,5 +147,6 @@ void	time_command(const std::string &line, const size_t &client_idx, const Serve
 void	motd_command(const std::string &line, const size_t &client_idx, const Server &serv);
 void	info_command(const std::string &line, const size_t &client_idx, const Server &serv);
 void	version_command(const std::string &line, const size_t &client_idx, const Server &serv);
+void	join_command(const std::string &line, const size_t &client_idx, const Server &serv);
 
 #endif
