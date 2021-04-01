c4be9d91e82a95337d70c10680f9c707beb1a239
EudaldV98 <janeudald98@gmail.com>
2021-04-01 15:38:23 +0200

    Adding part to quit command

--- srcs/commands/quit.cpp
+++ srcs/commands/quit.cpp
@@ -6,7 +6,7 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 12:27:36 by jvaquer           #+#    #+#             */
-/*   Updated: 2021/03/25 16:23:23 by jvaquer          ###   ########.fr       */
+/*   Updated: 2021/04/01 15:37:44 by jvaquer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,31 @@ void	quit_command(const std::string &line, const size_t &client_idx, const MySer
 {
 	std::vector<std::string> args;
 	std::string	output;
-
+	std::string	part_string;
+	
 	args = ft_split(line, " ");
+	for (int chann_idx = 0; chann_idx < g_vChannel.size(); chann_idx++)
+	{
+		for (std::vector<Client>::iterator it = g_vChannel[chann_idx]._users.begin();
+			it != g_vChannel[chann_idx]._users.end(); )
+		{
+			if (it->get_nickname() == g_aClient[client_idx].second.get_nickname())
+			{
+				part_string += g_vChannel[channel_idx].get_name() + ",";
+			}
+			else
+				it++;
+		}
+	}
+	std::cout << part_string << std::endl;
 	if (args.size() == 1)
 	{
-		// std::cout << "QUIT  :leaving" << std::endl; 
 		throw QuitCommandException();
 	}
 	else
 	{
 		for (int i = 1; i < args.size() ; i++)
 			output += args[i] + " ";
-		// std::cout << "QUIT   " << output << std::endl;
 		throw QuitCommandException();
 	}
 }
