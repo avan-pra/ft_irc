28ecff75f2eae5bebfbd3746f5cc09478d4160d8
Velovo <velovo.vanpraet@gmail.com>
2021-04-02 12:00:36 -0400

    add default switch for who command

--- srcs/create_msg.cpp
+++ srcs/create_msg.cpp
@@ -25,6 +25,8 @@ std::string create_msg(const int &code, const size_t &client_idx, const MyServ &
 			return sample + RPL_WELCOME(arg1);
 		case 221:
 			return sample + RPL_UMODEIS(arg1);
+		case 315:
+			return sample + RPL_ENDOFWHO(arg1);
 		case 322:
 			return sample + RPL_LIST(arg1, arg2);
 		case 323:
@@ -37,8 +39,10 @@ std::string create_msg(const int &code, const size_t &client_idx, const MyServ &
 			return sample + RPL_TOPIC(arg1, arg2);
 		case 341:
 			return sample + RPL_INVITING(arg1, arg2);
+		case 352:
+			return sample; //a faire wsh
 		case 353:
-			return sample + RPL_NAMREPLY(arg1, arg2); //modif_ici
+			return sample + RPL_NAMREPLY(arg1, arg2);
 		case 366:
 			return sample + RPL_ENDOFNAMES(arg1);
 		case 371:
