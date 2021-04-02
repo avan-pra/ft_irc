d9b81151060d840be5cb78cc17b6901397cadda5
Velovo <velovo.vanpraet@gmail.com>
2021-04-02 12:02:14 -0400

    convert space to tab

--- srcs/commands/who.cpp
+++ srcs/commands/who.cpp
@@ -4,20 +4,20 @@
 
 void	who_command(const std::string &line, const size_t &client_idx, const MyServ &serv)
 {
-    std::vector<std::string> arg = ft_split(line, " ");
+	std::vector<std::string> arg = ft_split(line, " ");
 
-    if (arg.size() < 2)
-        return;
-    switch (arg[1][0])
-    {
-        //query un channel
-        case '#':
-            //check si l'user est dans le channel
-            break;
-        //query un user
-        default:
-            break;
-    }
-    g_aClient[client_idx].second.send_reply(create_msg(315, client_idx, serv, arg[1]));
-    //352 RPL_WHOREPLY
+	if (arg.size() < 2)
+		return;
+	switch (arg[1][0])
+	{
+		//query un channel
+		case '#':
+			//check si l'user est dans le channel
+			break;
+		//query un user
+		default:
+			break;
+	}
+	g_aClient[client_idx].second.send_reply(create_msg(315, client_idx, serv, arg[1]));
+	//352 RPL_WHOREPLY
 }
