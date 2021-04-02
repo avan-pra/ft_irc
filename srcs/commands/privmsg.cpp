f60404b947304d2c6f40d03626ddcd6b9e750ed7
lmoulin <lucas.moulin78114@gmail.com>
2021-04-02 14:00:48 +0200

    privmsg fix

--- srcs/commands/privmsg.cpp
+++ srcs/commands/privmsg.cpp
@@ -6,7 +6,7 @@
 /*   By: lucas <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 11:33:44 by lucas             #+#    #+#             */
-/*   Updated: 2021/04/01 11:18:49 by lucas            ###   ########.fr       */
+/*   Updated: 2021/04/02 13:53:36 by lucas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,48 @@
 
 int		check_params(const std::vector<std::string> &params, const size_t &client_idx, const MyServ &serv)
 {
+	int		chan_id;
+
 	if (params.size() == 1)
 	{
+		//only command PRIVMSG had send
 		g_aClient[client_idx].second.send_reply(create_msg(411, client_idx, serv, params[0]));
 		return (0);
 	}
 	else if (params.size() == 2)
 	{
+		// No target had send
 		if (params[1][0] == ':')
 			g_aClient[client_idx].second.send_reply(create_msg(411, client_idx, serv, params[0] + " " + params[1]));
+		// No msg had send
 		else
 			g_aClient[client_idx].second.send_reply(create_msg(412, client_idx, serv));
 		return (0);
 	}
-	if (find_channel(params[1]) == -1 && find_user_by_nick(params[1]) == -1)
+	if ((chan_id = find_channel(params[1])) == -1 && find_user_by_nick(params[1]) == -1)
 	{
-		if (find_channel(params[1]) == -1)
-			g_aClient[client_idx].second.send_reply(create_msg(404, client_idx, serv, params[1]));
+		// No such channel
+		if (find_channel(params[1]) == -1 && (params[1][0] == '&' || params[1][0] == '#' ||
+			params[1][0] == '!' || params[1][0] == '+'))
+			g_aClient[client_idx].second.send_reply(create_msg(403, client_idx, serv, " " + params[1]));
+		// No such nickname
 		else
-			g_aClient[client_idx].second.send_reply(create_msg(401, client_idx, serv, params[1]));
+			g_aClient[client_idx].second.send_reply(create_msg(401, client_idx, serv, " " + params[1]));
 		return (0);
 	}
 	if (params[2][0] != ':')
 	{
+		//Msg not started by ':'
 		if (params[1][0] == ':')
 			g_aClient[client_idx].second.send_reply(create_msg(411, client_idx, serv, params[0] + " " + params[1]));
-			return (0);
+		return (0);
+	}
+	if (chan_id != -1 && g_vChannel[chan_id].get_mode().find("n") != std::string::npos &&
+	is_user_in_chan(chan_id, g_aClient[client_idx].second.get_nickname()) == false)
+	{
+		//User is not in the channel and the channel isn't set to accept extern messages
+		g_aClient[client_idx].second.send_reply(create_msg(404, client_idx, serv, params[1]));
+		return (0);
 	}
 	return (1);
 }
@@ -78,7 +94,9 @@ void	privmsg_command(const std::string &line, const size_t &client_idx, const My
 	params = ft_split(line, " ");
 	if (!check_params(params, client_idx, serv))
 		return ;
-	if ((i = find_channel(params[1])) != -1)
+	i = find_channel(params[1]);
+	std::cout << "coucou\n";
+	if (i != -1)
 		send_privmsg_to_channel(params, client_idx, serv, i);
 	else if ((i = find_user_by_nick(params[1])) != -1)
 		g_aClient[i].second.send_reply(create_full_msg(params, client_idx));
