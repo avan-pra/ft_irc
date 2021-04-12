09dce19b934dfbc77cee90469e8a8d1482df1d7f
Velovo <velovo.vanpraet@gmail.com>
2021-04-12 11:47:26 -0400

    starting who

--- srcs/commands/who.cpp
+++ srcs/commands/who.cpp
@@ -2,6 +2,28 @@
 #include "../../includes/MyServ.hpp"
 #include "../../includes/commands.hpp"
 
+typedef size_t zize_t;
+
+//return -1 si il y a eu un prblm
+int	who_channel(const std::string &channel, const size_t &client_idx)
+{
+	if (channel.find("*", 0) != std::string::npos)
+		return (-1);
+	
+	int channel_id = find_channel(channel);
+
+	if (channel_id < 0)
+		return (1);
+	if (g_aClient[client_idx].second.get_is_oper() == false && find_user_in_channel(channel, g_aClient[client_idx].second.get_nickname()) == g_vChannel[channel_id]._users.end())
+		return (1);
+	//std::cout << "who passed" << std::endl;
+	for (zize_t n = 0; n < g_vChannel[channel_id]._users.size(); ++n)
+	{
+		;
+	}
+	return (0);
+}
+
 void	who_command(const std::string &line, const size_t &client_idx, const MyServ &serv)
 {
 	std::vector<std::string> arg = ft_split(line, " ");
@@ -12,8 +34,10 @@ void	who_command(const std::string &line, const size_t &client_idx, const MyServ
 	{
 		//query un channel
 		case '#':
-			//check si l'user est dans le channel
+		{
+			who_channel(arg[1], client_idx);
 			break;
+		}
 		//query un user
 		default:
 			break;
@@ -23,7 +47,7 @@ void	who_command(const std::string &line, const size_t &client_idx, const MyServ
 }
 /*
 WHO #bonjour
-
+						   channel	user   hosts			 server		  nick
 :oragono.test 352 avan-pra #bonjour ~u rnkgc4kxqgj2n.irc oragono.test idk H@r :0 idk
 :oragono.test 352 avan-pra #bonjour ~u s23g99j2hmmg2.irc oragono.test avan-pra H :0 avan-pra
 :oragono.test 315 avan-pra #bonjour :End of WHO list
