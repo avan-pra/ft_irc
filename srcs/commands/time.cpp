b26732d49d243a0349a10684053e542f9aa947f6
lmoulin <lucas.moulin78114@gmail.com>
2021-04-22 16:12:32 +0200

    fix time cpp98

--- srcs/commands/time.cpp
+++ srcs/commands/time.cpp
@@ -6,7 +6,7 @@
 /*   By: lucas <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 14:54:27 by lucas             #+#    #+#             */
-/*   Updated: 2021/03/22 22:01:53 by lucas            ###   ########.fr       */
+/*   Updated: 2021/04/22 16:11:29 by lucas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	time_command(const std::string &line, const size_t &client_idx, const MySer
 	(void)line;
 	time(&get_time);
 	now = ctime(&get_time);
-	if (now.back() == '\n')
-		now.pop_back();
+	if (now[now.size() - 1] == '\n')
+		now.resize(now.size() - 1);
 	g_aClient[client_idx].second.send_reply(RPL_TIME(serv.get_hostname(), now.c_str()));
 }
