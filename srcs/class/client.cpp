89c7d9488d9f423daf6f628d0e8bcc478e109d97
EudaldV98 <janeudald98@gmail.com>
2021-04-29 22:55:58 +0200

    fixed bug in client destructor (erase of channel)

--- srcs/class/client.cpp
+++ srcs/class/client.cpp
@@ -14,8 +14,8 @@ static void tmp_send_to_channel(const std::string &msg, Client &cli, Channel &ch
 
 Client::~Client()
 {
-	for (std::deque<Channel>::iterator cht = g_vChannel.begin(); cht != g_vChannel.end(); ++cht)
-	{	
+	for (std::deque<Channel>::iterator cht = g_vChannel.begin(); cht != g_vChannel.end(); )
+	{
 		if (cht->is_user_in_chan(*this) == true)
 		{
 			cht->remove_user(this->get_nickname());
@@ -23,9 +23,9 @@ Client::~Client()
 			cht->remove_user_voice(this->get_nickname());
 			tmp_send_to_channel("PART " + cht->get_name() + " :", *this, *cht);
 			if (cht->_users.size() == 0)
-			{
 				cht = g_vChannel.erase(cht);
-			}
+			else
+				cht++;
 		}
 	}
 }
