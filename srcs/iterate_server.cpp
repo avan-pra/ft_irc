3882abadb4356cb0f3bed4c456fb86b92d9e3cb7
Velovo <velovo.vanpraet@gmail.com>
2021-05-12 09:11:33 -0400

    copy paste clioent into server

--- srcs/iterate_server.cpp
+++ srcs/iterate_server.cpp
@@ -5,27 +5,37 @@
 
 void	iterate_server(MyServ &serv)
 {
-	char c[BUFF_SIZE + 1];
+	char	c[BUFF_SIZE + 1];
+	int		ret = 0;
 
 	for (std::list<Server>::iterator it = g_all.g_aServer.begin(); it != g_all.g_aServer.end(); ++it)
 	{
+		//std::cout << "in iterate client i =" << i << std::endl;
 		ping_if_away(*it, serv);
 		//si je l'ai kick car ca fait trop longtemps qu'il a pas rep alors forcement je vais pas check ses demandes
-		if (kick_if_away(*it, serv) == true)
-			disconnect(&(*it), it);
-		else if (FD_ISSET(it->_fd, &serv.get_readfs()))
+		if (kick_if_away(*it, serv) == true || check_register_timeout(*it, serv) == true)
 		{
-			ft_bzero((char *)c, sizeof(c));
-			int ret = recv(it->_fd, &c, BUFF_SIZE, 0);
+			//std::cout << i << "= i; before disconnect if away {" << g_aClient[i].second.get_nickname() << "}\n";
+			disconnect(&(*it), it);
+			//std::cout << i << "= i; after disconnect if away \n";
+		}
+		else if (is_readable(serv, *it))
+		{
+			get_message(c, *it, ret);
+			check_message_problem(c, *it, serv, ret);
+			/*
+			** get_message & check_message_problem may set ret to -1 which indicate an critical error such as a too big packet size
+			** an ssl handshake error, read error or if the client isnt writeable
+			*/
 			if (ret <= 0)
 				disconnect(&(*it), it);
 			else if (ret > 0)
 			{
-				c[ret] = '\0';
 				try
 				{
-					;
+					;//parser(c, it, serv);
 				}
+				catch (const DieException &e) { throw DieException(); }
 				catch(const IncorrectPassException &e) { disconnect(&(*it), it); }
 				catch(const QuitCommandException &e) { disconnect(&(*it), it); }
 			}
