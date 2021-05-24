ed32207e10d2bfd86769d7f3db80838ea123f8e3
EudaldV98 <janeudald98@gmail.com>
2021-05-24 15:53:53 +0200

    Notice Service

--- includes/IRCserv.hpp
+++ includes/IRCserv.hpp
@@ -6,7 +6,7 @@
 /*   By: jvaquer <jvaquer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 18:15:22 by lucas             #+#    #+#             */
-/*   Updated: 2021/05/21 18:41:34 by jvaquer          ###   ########.fr       */
+/*   Updated: 2021/05/24 15:53:18 by jvaquer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -247,6 +247,7 @@ bool								is_servername_exist(const std::string &servname);
 std::string		create_full_msg(const std::vector<std::string> &params, std::list<Client>::iterator client_it);
 std::string		create_full_msg_mode(const std::string &mode, std::list<Client>::iterator client_it, const size_t &chann_idx);
 std::string		create_full_name_msg(std::list<Client>::iterator client_it);
+std::string		create_full_msg(const std::vector<std::string> &params, std::list<Service>::iterator service_it);
 
 /*
 ** mask_parser.cpp
