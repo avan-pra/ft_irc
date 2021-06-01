e060a53b5489d027ac3f5c16934522d937e802b7
lmoulin <lucas.moulin78114@gmail.com>
2021-06-01 02:28:22 +0200

    add version multi server

--- srcs/commands/version.cpp
+++ srcs/commands/version.cpp
@@ -6,7 +6,7 @@
 /*   By: lucas <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 22:39:14 by lucas             #+#    #+#             */
-/*   Updated: 2021/05/24 17:17:03 by lucas            ###   ########.fr       */
+/*   Updated: 2021/06/01 01:02:24 by lucas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,10 @@ void	version_other_serv(std::string &serv_name, std::list<Client>::iterator clie
 		client_it->push_to_buffer(create_msg(402, client_it, serv, serv_name));
 		return ;
 	}
-	server_it->push_to_buffer(":" + client_it->get_nickname() + " VERSION " + serv_name + "\r\n");
+	if (server_it->get_hopcount() > 1)
+		server_it->get_server_uplink()->push_to_buffer(":" + client_it->get_nickname() + " VERSION " + serv_name + "\r\n");
+	else
+		server_it->push_to_buffer(":" + client_it->get_nickname() + " VERSION " + serv_name + "\r\n");
 }
 
 void	version_command(const std::string &line, std::list<Client>::iterator client_it, const MyServ &serv)
@@ -43,6 +46,7 @@ void	version_command(const std::string &line, std::list<Server>::iterator server
 {
 	std::vector<std::string>	params = ft_split(line, " ");
 	std::list<Client>::iterator	client_it;
+	std::list<Server>::iterator	serv_cible;
 
 	if (params.size() < 3)
 		return ;
@@ -50,4 +54,16 @@ void	version_command(const std::string &line, std::list<Server>::iterator server
 		return ;
 	if (params[2] == serv.get_hostname())
 		server_it->push_to_buffer(create_msg(351, client_it, serv, std::string("beta 1.0"), "0", serv.get_hostname(), std::string("Actually in beta 1.0")));
+	else
+	{
+		if ((serv_cible = find_server_by_iterator(params[2])) == g_all.g_aServer.end())
+		{
+			server_it->push_to_buffer(create_msg(402, client_it, serv, params[2]));
+			return ;
+		}
+		if (serv_cible->get_hopcount() > 1)
+			serv_cible->get_server_uplink()->push_to_buffer(line + "\r\n");
+		else
+			serv_cible->push_to_buffer(line + "\r\n");
+	}
 }
